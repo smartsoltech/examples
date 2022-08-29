bf3d518eccfad556cae1179a878b47592b4c2032
Andrew Choi <a.choi@smartsoltech.kr>
2022-08-29 21:21:41 +0900

    added main functional code

--- example_c#_Hello_world/Program.cs
+++ example_c#_Hello_world/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Andrew");
+﻿// First probe program
+Console.WriteLine("Enter your name> ");
+string name = Console.ReadLine();
+Console.Write("Hello, ");
+Console.WriteLine(name);
